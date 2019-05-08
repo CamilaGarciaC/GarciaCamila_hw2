8fdd1227ebd47b9c5b0720a03b63a8c355784304
Camila Garcia <mariac_412@hotmail.com>
2019-05-08 14:02:20 -0500

    avance

--- Edificio.cpp
+++ Edificio.cpp
@@ -5,13 +5,22 @@
 
 using namespace std;
 
+
 //Se declaran los parametros.
 const double m = 1000.0;
 const double k = 2000.0;
 const double gama = 0.0;
 const double h = 0.1;
 
-//Funciones para el metodo de runge kutta
+//Funciones de derivada para el metodo de runge kutta
 double du1dt( double t, double u1, double u2, double u3, double v1, double v2, double v3){
 	return v1;
+}
+
+double du2dt( double t, double u1, double u2, double u3, double v1, double v2, double v3){
+	return v2;
+}
+
+double du3dt( double t, double u1, double u2, double u3, double v1, double v2, double v3){
+	return v3;
 }
