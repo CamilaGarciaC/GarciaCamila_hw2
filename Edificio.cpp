f8da57a83b8fcf9d7cde22fd61973099acd6824b
Camila Garcia <mariac_412@hotmail.com>
2019-05-08 19:03:07 -0500

    se corrigio el main

--- Edificio.cpp
+++ Edificio.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cmath>
-//#include <fstream>
-//#include <vector>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+#define TAB "\t"
 
 //Se declaran los parametros.
 const double m = 1000.0;
@@ -40,7 +41,7 @@ double dv3dt( double t, double u1, double u2, double u3, double v1, double v2, d
 }
 
 //Metodo runge kutta
-void rk4_step( double t, double &u1, double &u2, double &u3, double &v1, double &v2, double &v3, double w ){
+void rungekutta( double t, double &u1, double &u2, double &u3, double &v1, double &v2, double &v3, double w ){
 	double k[4][6];
 
 	k[0][0] = h*du1dt( t, u1, u2, u3, v1, v2, v3 );
@@ -77,4 +78,66 @@ void rk4_step( double t, double &u1, double &u2, double &u3, double &v1, double 
 	v1 += ( k[0][3] + 2*(k[1][3] + k[2][3]) + k[3][3] )/6.0;
 	v2 += ( k[0][4] + 2*(k[1][4] + k[2][4]) + k[3][4] )/6.0;
 	v3 += ( k[0][5] + 2*(k[1][5] + k[2][5]) + k[3][5] )/6.0;
+}
+
+//Se crea funcion que halla los ui(t)max
+
+double maximo( double* arr, int size ){
+	int i, j;
+	double tmp;
+
+	for( i = 1; i < size; i++ ){
+		for( j = 0; j < size-1; j++ ){
+			if( arr[j] > arr[j+1] ){
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
+
+	return arr[size-1];
+}
+
+//Main que crea los archivos de datos a ser graficados 
+int main(int argc, char *argv[]){
+	ofstream data;
+	data.open("4graficas.dat");
+
+	double w = sqrt(k/m);
+	double u1 = 0.0, u2 = 0.0, u3 = 0.0, v1 = 0.0, v2 = 0.0, v3 = 0.0;
+	//Agregan los datos para las graficas de ui(t) en funcion del tiempo
+	for( double t = 0.0; t < 200; t += h ){
+		rungekutta( t, u1, u2, u3, v1, v2, v3, w );
+		data << t << TAB << u1 << TAB << u2 << TAB << u3 << TAB << v1 << TAB << v2 << TAB << v3 << endl;
+	}	
+	data.close();
+	//Se agregan los datos para la grafica de ui(t)max
+	ofstream data2;
+	data2.open("1grafica.dat");
+
+	double wmin = 0.2*sqrt(k/m);
+	double wmax = 3.0*sqrt(k/m);
+	double dw = (wmax-wmin)/99.0;
+	
+	for( double W = wmin; W < wmax; W += dw ){
+		double u1 = 0.0, u2 = 0.0, u3 = 0.0, v1 = 0.0, v2 = 0.0, v3 = 0.0;
+		vector<double> x1, x2, x3;
+
+		for( double t = 0.0; t < 200; t += h ){
+			rungekutta( t, u1, u2, u3, v1, v2, v3, W );
+		
+			x1.push_back(u1);
+			x2.push_back(u2);
+			x3.push_back(u3);
+		}
+
+		double u1_max = maximo( x1.data(), x1.size() );
+		double u2_max = maximo( x2.data(), x2.size() );
+		double u3_max = maximo( x3.data(), x3.size() );
+		data2 << W << TAB << u1_max << TAB << u2_max << TAB << u3_max << endl;
+	}
+	data2.close();
+
+	return 0;
 }
